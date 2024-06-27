0d1221f922c4d0125cbf687962057fb2c1a8b8ce
efa07 <efatariku07@gmail.com>
2024-06-27 21:21:54 +0300

    the code updated to more efficient way

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,16 +6,16 @@
 def pascal_triangle(n):
     if n <= 0:
         return []
-    
-    triangle = [[1]]
-    
-    for i in range(1, n):
-        row = [1]
+
+    triangle = []
+
+    for i in range(n):
+        # Initialize the row with 1's
+        row = [1] * (i + 1)
+        # Update the elements in the middle of the row
         for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
+            row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+        # Append the completed row to the triangle
         triangle.append(row)
-    
+
     return triangle
-
-
