225f129fbfebb54d45956b6de26b6df194c3d31f
efa07 <efatariku07@gmail.com>
2024-07-04 21:46:23 +0300

    PEP 8 style added

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,27 +4,41 @@
 def canUnlockAll(boxes):
     """
     Determine if all boxes can be unlocked.
-    Each box contains keys to other boxes, and the goal is to figure
+    Each box contains keys to other boxes, and the goal is to figure 
     out if we can unlock all the boxes starting from box 0.
 
     Args:
-        boxes (List[List[int]]): A list of lists where each list represents
+        boxes (List[List[int]]): A list of lists where each list represents 
         a box and contains the keys to other boxes.
     Returns:
         bool: True if all boxes can be unlocked, False otherwise.
     """
-    
-    if not isinstance(boxes, list):
-        return False
-    elif len(boxes) == 0:
+
+    # Check if there are no boxes to work with
+    if not boxes:
         return False
 
-    for k in range(1, len(boxes) - 1):
-        boxes_checked = False
-        for idx in range(len(boxes)):
-            boxes_checked = k in boxes[idx] and k != idx
-            if boxes_checked:
-                break
-        if boxes_checked is False:
-            return boxes_checked
-    return True
+    # Number of boxes
+    n = len(boxes)
+    # Initialize a list to keep track of which boxes have been opened
+    opened = [False] * n
+
+    # The first box (index 0) is always unlocked
+    opened[0] = True
+
+    def dfs(box):
+        """
+        Perform a depth-first search to unlock boxes.
+        Args:
+            box (int): The index of the current box being processed.
+        """
+        # Iterate over the keys found in the current box
+        for key in boxes[box]:
+            # Check if the key is within the valid range and the box has not been opened yet
+            if key < n and not opened[key]:
+                opened[key] = True
+                dfs(key)
+
+    dfs(0)
+
+    return all(opened)
