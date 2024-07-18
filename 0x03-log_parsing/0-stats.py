9b5ca5df12c9b6fecd68f4628beae960dbcd2a9e
efa07 <efatariku07@gmail.com>
2024-07-18 21:19:08 +0300

    whole code changed

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,40 +3,46 @@
 log parsing
 """
 
+import sys
+import re
 
-from collections import defaultdict
+
+def output(total_size: int, line_count: int, status_counts: dict) -> None:
+  """
+  helper function to display stats
+  """
+  print(f"Total file size: {total_size}")
+  for code, count in sorted(status_counts.items()):
+    if count:
+      print(f"{code}: {count}")
 
 
-# Define valid status codes
-VALID_STATUS_CODES = {200, 301, 400, 401, 403, 404, 405, 500}
+if __name__ == "__main__":
+  regex = re.compile(
+      r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\]"GET /projects/260 HTTP/1.1" (.{3}) (\d+)')
 
-total_size = 0
-line_count = 0
-status_counts = defaultdict(int)
+  line_count = 0
+  total_size = 0
+  status_counts = {str(code): 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]}
 
-try:
-    for line in iter(input, ""):
+  try:
+    for line in sys.stdin:
+      line = line.strip()
+      match = regex.fullmatch(line)
+      if match:
         line_count += 1
+        code = match.group(1)
+        file_size = int(match.group(2))
 
-        parts = line.split()
-
-        if len(parts) != 6 or not parts[0].isdigit() or not
-        parts[-2].isdigit():
-            continue
-
-        file_size = int(parts[-1])
-
+        # Update metrics
         total_size += file_size
-        status_counts[int(parts[-2])] += 1
+        status_counts[code] += 1
 
+        # Print statistics every 10 lines or on interruption
         if line_count % 10 == 0 or line_count == 1:
-            print(f"Total file size: {total_size}")
-            for code, count in sorted(status_counts.items()):
-                if code in VALID_STATUS_CODES:
-                    print(f"{code}: {count}")
+          output(total_size, line_count, status_counts.copy())  # Avoid modifying original dict
 
-except KeyboardInterrupt:
-    print(f"Total file size: {total_size}")
-    for code, count in sorted(status_counts.items()):
-        if code in VALID_STATUS_CODES:
-            print(f"{code}: {count}")
+  except KeyboardInterrupt:
+    # Print statistics on interruption
+    output(total_size, line_count, status_counts.copy())
+
