bdf4d5d5d5c1575d61ccbb8358f3ccafee42a95d
efa07 <efatariku07@gmail.com>
2024-07-25 22:56:23 +0300

    time and space complexity fixed

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,36 +1,39 @@
 #!/usr/bin/python3
+"""UTF-8 Validation"""
 
+
+def count_leading_ones(byte):
+    """Returns the number of leading 1s in the given byte."""
+    leading_ones = 0
+    mask = 1 << 7
+    while mask & byte:
+        leading_ones += 1
+        mask >>= 1
+    return leading_ones
 
 def validUTF8(data):
-    """UTF-8 Validtor"""
+    """
+    Determines if a given data set represents a valid UTF-8 encoding.
 
-    n_bytes = 0
+    Args:
+        data (list): List of integers representing bytes of data.
 
-    # Masks to check the significant bits
-    mask1 = 1 << 7  # 10000000
-    mask2 = 1 << 6  # 01000000
-
-    for num in data:
-        # Extract the 8 least significant bits of num
-        num = num & 0xFF
-
-        if n_bytes == 0:
-            # Determine the number of bytes in the UTF-8 character
-            if (num & mask1) == 0:
-                # 1-byte character (0xxxxxxx)
+    Returns:
+        bool: True if data is a valid UTF-8 encoding, else False.
+    """
+    remaining_bytes = 0
+    for byte in data:
+        if remaining_bytes == 0:
+            remaining_bytes = count_leading_ones(byte)
+            # 1-byte character (0xxxxxxx)
+            if remaining_bytes == 0:
                 continue
-            elif (num & (mask1 >> 1)) == mask1:
-                n_bytes = 1
-            elif (num & (mask1 >> 2)) == (mask1 >> 1):
-                n_bytes = 2
-            elif (num & (mask1 >> 3)) == (mask1 >> 2):
-                n_bytes = 3
-            else:
+            # A character in UTF-8 can be 1 to 4 bytes long
+            if remaining_bytes == 1 or remaining_bytes > 4:
                 return False
         else:
-            if not (num & mask1 and not (num & mask2)):
+            # Check if the byte has the format 10xxxxxx
+            if not (byte & (1 << 7) and not (byte & (1 << 6))):
                 return False
-
-        n_bytes -= 1
-
-    return n_bytes == 0
+        remaining_bytes -= 1
+    return remaining_bytes == 0
