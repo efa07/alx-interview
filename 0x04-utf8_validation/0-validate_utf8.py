14085d53d43225460167aa534f0d100c9dcc1269
efa07 <efatariku07@gmail.com>
2024-07-25 22:52:13 +0300

    time and space complexity fixed

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,30 +2,35 @@
 
 
 def validUTF8(data):
-    """UTF-8 Validation"""
-    # Number of bytes in the current UTF-8 character
+    """UTF-8 Validtor"""
+
     n_bytes = 0
 
     # Masks to check the significant bits
-    mask1 = 1 << 7
-    mask2 = 1 << 6
+    mask1 = 1 << 7  # 10000000
+    mask2 = 1 << 6  # 01000000
 
     for num in data:
-        mask = 1 << 7
+        # Extract the 8 least significant bits of num
+        num = num & 0xFF
+
         if n_bytes == 0:
             # Determine the number of bytes in the UTF-8 character
-            while mask & num:
-                n_bytes += 1
-                mask >>= 1
-            # 1-byte character
-            if n_bytes == 0:
+            if (num & mask1) == 0:
+                # 1-byte character (0xxxxxxx)
                 continue
-            # Invalid scenarios according to UTF-8 encoding rules
-            if n_bytes == 1 or n_bytes > 4:
+            elif (num & (mask1 >> 1)) == mask1:
+                n_bytes = 1
+            elif (num & (mask1 >> 2)) == (mask1 >> 1):
+                n_bytes = 2
+            elif (num & (mask1 >> 3)) == (mask1 >> 2):
+                n_bytes = 3
+            else:
                 return False
         else:
             if not (num & mask1 and not (num & mask2)):
                 return False
+
         n_bytes -= 1
 
     return n_bytes == 0
